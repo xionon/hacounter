ceeb8b308f9f269c75d3ecaa1bbc5bdf60a998db
Alec Hipshear <alec.hipshear@getbraintree.com>
2015-01-23 13:55:41 -0500

    Reorg basic json for consul config

--- scripts/consul_node.sh
+++ scripts/consul_node.sh
@@ -6,7 +6,7 @@ echo "Provisioning consul_node"
 ip=$1
 
 sudo mkdir -p /etc/consul.d/agent
-sudo cp /vagrant/counter/config.json /etc/consul.d/agent/config.json
+sudo cp /vagrant/scripts/templates/consul_node_config.json /etc/consul.d/agent/config.json
 sudo sed -i'' -e "s/\:ip\:/$ip/" /etc/consul.d/agent/config.json
 
 sudo foreman export upstart /etc/init --procfile /vagrant/consul/Procfile.consul-agent --user vagrant --app consul
--- scripts/consul_server.sh
+++ scripts/consul_server.sh
@@ -6,7 +6,7 @@ echo "Provisioning consul_server"
 ip=$1
 
 sudo mkdir -p /etc/consul.d/server
-sudo cp /vagrant/consul/config.json /etc/consul.d/server/config.json
+sudo cp /vagrant/scripts/templates/consul_server_config.json /etc/consul.d/server/config.json
 sudo sed -i'' -e "s/\:ip\:/$ip/" /etc/consul.d/server/config.json
 
 sudo foreman export upstart /etc/init --procfile /vagrant/consul/Procfile.consul-server --user vagrant --app consul
