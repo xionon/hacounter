4595ada883c9d2d9098702a78c247010646b23a5
Anthony J Burton <anthony.burton@venmo.com>
2015-01-26 10:32:36 -0500

    initial pass (which works with ssh config modification) to add web ui

--- scripts/consul_server.sh
+++ scripts/consul_server.sh
@@ -9,5 +9,12 @@ sudo mkdir -p /etc/consul.d/server
 sudo cp /vagrant/scripts/templates/consul_server_config.json /etc/consul.d/server/config.json
 sudo sed -i'' -e "s/\:ip\:/$ip/" /etc/consul.d/server/config.json
 
+cd /tmp
+sudo mkdir -p /var/lib/consul
+wget --quiet https://dl.bintray.com/mitchellh/consul/0.4.1_web_ui.zip
+unzip 0.4.1_web_ui.zip
+sudo mv dist /var/lib/consul/web_ui
+sudo chown -R vagrant:vagrant /var/lib/consul
+
 sudo foreman export upstart /etc/init --procfile /vagrant/consul/Procfile.consul-server --user vagrant --app consul
 sudo service consul restart
